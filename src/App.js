215a9bdd721d61ac9c804a0a23bf39a7247e886f
Maitrik T <maitrikt1998@gmail.com>
2024-02-25 21:39:29 +0530

    Basic Redux To do

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, toggleTodo, editTodo, deleteTodo } from './todoSlice';
 
 function App() {
+  const [ text, setText ] = useState('');
+  const [editId, setEditId] = useState(null);
+  const [editText, setEditText] = useState('');
+  const todos = useSelector(state => state.todos);
+  const dispatch = useDispatch();
+  
+  const handleAddTodo = () =>{
+    if( text.trim !== ''){
+      dispatch(addTodo(text));
+      setText('');
+    }
+  }
+
+  const handleEditStart = (id, text) =>{
+    setEditId(id);
+    setEditText(text);
+  }
+
+  const handleEditCancel = () => {
+    setEditId(null);
+    setEditText('');
+  }
+
+  const handleEditSave = () => {
+    if (editText.trim() !== '') { 
+      dispatch(editTodo({id:editId, newtext:editText}))
+    }
+    setEditId(null);
+    setEditText('');
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' value={text} onChange={ e=> setText(e.target.value) } placeholder='Enter a New Todo' />
+      <button onClick= {handleAddTodo} >Add Todo</button>
+      <ul>
+        {todos.map(todo=>(
+          <li key={todo.id}>
+            {editId === todo.id ? (
+                <>
+                <input type='text' value={editText} onChange={e=> setEditText(e.target.value)} />
+                <button onClick={handleEditSave}>Save</button>
+                <button onClick={handleEditCancel}>Cancel</button>
+                </>
+              ):(
+                <>
+                <input type='checkbox' checked={todo.completed} onChange={()=>dispatch(toggleTodo(todo.id))} />
+                <span style={{ textDecoration: todo.completed ? 'line-through': 'none' }}>
+                  {todo.text}
+                </span>
+                <button onClick={()=> handleEditStart(todo.id, todo.text)}>Edit</button>
+                <button onClick={()=> dispatch(deleteTodo(todo.id))}>Delete</button>
+                </>
+              )
+            }
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
